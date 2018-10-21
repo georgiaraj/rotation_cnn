ddd4584d386daefccfe9e356b8887cd92f28bce5
Georgia Rajamanoharan <georgia.rajamanoharan@crowdvision.co.uk>
2018-10-21 15:33:17 +0100

    Remove all the fully convolutional layer stuff and simplify in preparation for rotation task.

--- rot_cnn.py
+++ rot_cnn.py
@@ -15,17 +15,11 @@ import math
 class cnn(object):
     ###############################################################################
     # Methods currently established as beta/stable
-    def __init__(self, model_name, model_path, im_size, use_fullconv=False, batch_size=128, nepochs=100, use_bn=True, lr=0.001, decay=1e-5, pdrop_conv=0., pdrop_fc=0., fc_layers=[512], fcn_head_row=None, fcn_head_col=None, padding='same'):
-        # Sanity check for fully convolutional
+    def __init__(self, model_name, model_path, im_size, batch_size=128, nepochs=100, use_bn=True, lr=0.001, decay=1e-5, pdrop_conv=0., pdrop_fc=0., fc_layers=[512], fcn_head_row=None, fcn_head_col=None, padding='same'):
 
         if padding != 'same' and padding != 'valid':
             print('ParamError: Conv2D padding can either be \'valid\' or \'same\'.')
             sys.exit(0)
-
-        if use_fullconv:
-            mode_fullconv = '_fullconv'
-        else:
-            mode_fullconv = ''
 
         if padding == 'same':
             padding_text = '_'+padding
@@ -44,9 +38,6 @@ class cnn(object):
         self.pdrop_conv = pdrop_conv # percentage of neurons dropped in convolution layers, wherever Dropout is used; if set to '0', no dropout occurs
         self.pdrop_fc = pdrop_fc # percentage of neurons dropped in fully connected layer; if set to '0', no dropout occurs
         self.fc_layers = fc_layers # List of the number of nodes per fully connected inner layer
-        self.use_fullconv = use_fullconv
-        self.fcn_head_row = fcn_head_row
-        self.fcn_head_col = fcn_head_col
         self.padding = padding
 
         self.model = None
@@ -55,11 +46,11 @@ class cnn(object):
 
     def train(self, images, labels):
         # Divide data into training and validation samples in ratio 4:1
-        ntrain = round(patches.shape[0]*4/5)
-        patches_train = np.expand_dims(patches[0:ntrain,:,:],axis=3)
+        ntrain = round(images.shape[0]*4/5)
+        patches_train = np.expand_dims(images[0:ntrain,:,:],axis=3)
         labels_train = labels[0:ntrain,:]
-        patches_val = np.expand_dims(patches[ntrain:patches.shape[0],:,:],axis=3)
-        labels_val = labels[ntrain:patches.shape[0],:]
+        patches_val = np.expand_dims(images[ntrain:images.shape[0],:,:],axis=3)
+        labels_val = labels[ntrain:images.shape[0],:]
 
         # Setup in the tensorflow session
         sess = tf.Session()
@@ -67,22 +58,12 @@ class cnn(object):
 
         # Build the graph
         #img = tf.placeholder(tf.float32, shape=(None, patch_size))
-        nrows = patches.shape[1]
-        ncols = patches.shape[2]
+        nrows = images.shape[1]
+        ncols = images.shape[2]
         nchannels = 1
-        use_custom_fcn_regressor_head = False
 
-        # Experimental code. Inference logic to build FCN graph with dynamic input; Requires manually setting the head of the FCN regressor
-        if self.use_fullconv and self.fcn_head_col is not None and self.fcn_head_row is not None:
-             nrows = None
-             ncols = None
-             use_custom_fcn_regressor_head = True
-
-        patch_shape = (nrows, ncols, nchannels)
-        if use_custom_fcn_regressor_head:
-            model = self.__build_graph(patch_shape, padding=self.padding, fcn_head_row=self.fcn_head_row, fcn_head_col=self.fcn_head_col)
-        else:
-            model = self.__build_graph(patch_shape, padding=self.padding)
+        image_shape = (nrows, ncols, nchannels)
+        model = self.__build_graph(image_shape, padding=self.padding)
 
         model.summary() #print neural network summary
 
@@ -99,15 +80,6 @@ class cnn(object):
         print(opt.get_config())
         model.compile(loss='mean_squared_error', optimizer=opt, metrics=['mae'])
 
-        # Actually do the training; If fully convolutional reshape labels_train
-        if self.use_fullconv:
-            str_shape_original = '{0}'.format(labels_train.shape)
-            labels_train = np.reshape(labels_train, (labels_train.shape[0], 1, 1, labels_train.shape[1]))
-            labels_val = np.reshape(labels_val, (labels_val.shape[0], 1, 1, labels_val.shape[1]))
-            str_shape_new = '{0}'.format(labels_train.shape)
-            print('Using Fully Convolutional Network')
-            print('Reshaping \'labels_train\' and \'labels_val\'  from: ' + str_shape_original + ' to: ' + str_shape_new)
-
         model.fit(patches_train, labels_train, batch_size=self.batch_size, epochs=self.nepochs, validation_data=(patches_val, labels_val), shuffle=True, callbacks=callbacks)
 
         # Save the model
@@ -122,44 +94,6 @@ class cnn(object):
             print('Model',self.model_path,'not trained yet.')
             return False
 
-    def reshape_input_layer_full_convnet(self, new_patch_shape, padding='same', verbose=False):
-        # Check that we are using a fully convolutional network
-        if self.is_trained() == False or self.use_fullconv == False:
-            print('Error. \'reshape_input_layer_full_convnet\' is only supported for trained Full ConvNets2D')
-            sys.exit(0)
-
-        original_shape = self.model.layers[0].input_shape
-        # Check that original trained network input layer is trained for dynamic input (this is whan input layer shape (None, None, None, 1)
-        # If trained for dynamic input, no reshaped/new network is required
-        if original_shape[1] is None and original_shape[2] is None:
-            return
-
-        # Check whether it is necessary to reshape based on expected images' rows and cols
-        if new_patch_shape[0:2] == original_shape[1:3]:
-            return
-
-        # Find head of fcn shape(nrows,ncols) from loaded/trained network. This would be the first Layer2D that is not (None,1,1,None)
-        # This is provided that no Maxpooling or strides>1 etc techniques were used on the fcn part of the network
-        fcn_head_row = None
-        fcn_head_col = None
-        for layer in reversed(self.model.layers):
-            if layer.output_shape[1] != 1:
-                fcn_head_row = layer.output_shape[1]
-                fcn_head_col = layer.output_shape[2]
-                break
-        new_model = self.__build_graph(new_patch_shape, padding, fcn_head_row, fcn_head_col)
-        #Replace weights and reset model
-        for n, layer in  enumerate(self.model.layers):
-            new_model.layers[n].set_weights(layer.get_weights())
-
-        if verbose:
-             print('Original Full-ConvNet summary')
-             self.model.summary()
-             print('Reshaped Full-ConvNet summary')
-             new_model.summary()
-
-        self.model = new_model
-
     def set_session(self):
         sess = tf.Session()
         K.set_session(sess)
@@ -172,96 +106,12 @@ class cnn(object):
         self.model.summary()
 
     def test(self, patches, use_beta=False):
-        # Prepare network for variable size images onthe FullConvNet case
-        if self.use_fullconv:
-            if use_beta is False:
-                print('Error in \'cnn.test\'. Fully ConvNets are still beta. Set \'use_beta=True\' to test.')
-                exit(0)
-            #Check that input images require for the network to reset its input layer; check is done inside reshape method
-            nrows = patches.shape[1]
-            ncols = patches.shape[2]
-            nchannels = 1
-            new_shape = (nrows, ncols, nchannels)
-            self.reshape_input_layer_full_convnet(new_shape, padding=self.padding, verbose=False)
         labels = self.model.predict(patches, batch_size=self.batch_size, verbose=1)
 
         return labels
 
-    def test_shifted_images(self, image):
-        if self.use_fullconv is False:
-            print('ParamError: \'test_shifted_images\' is only supported for FullyConvNets.')
-            sys.exit(0)
-        if image.shape[0] != 1:
-            print('ParamError: \'test_shifted_images\' currently only supports one query image at a time.')
-            sys.exit(0)
-
-        # 1. Check whether the input images require for the network to reset its input layer; check is done inside reshape method
-        n_rows_out = image.shape[1]
-        n_cols_out = image.shape[2]
-        nchannels = 1
-        new_shape = (n_rows_out, n_cols_out, nchannels)
-        self.reshape_input_layer_full_convnet(new_shape, padding='same', verbose=False)
-
-        # Calculate the scale factor from the network architecture
-        scaling_factor = self.get_scaling_factor()
-
-        # Compute all slided regression maps.
-        ll_labels = []
-        for i_r_slide in range(scaling_factor):
-            l_labels = []
-            for i_c_slide in range(scaling_factor):
-                # Current slide map to query cnn
-                cur_image = np.zeros(image.shape)
-                cur_image[:, 0:n_rows_out-i_r_slide, 0:n_cols_out-i_c_slide, :] = image[:, i_r_slide:, i_c_slide:, :]
-                labels = self.model.predict(cur_image, batch_size=2, verbose=1)
-                l_labels.append(labels)
-            ll_labels.append(l_labels)
-
-        return ll_labels
-
     def get_cnn(self):
         return self.model
-
-    def get_scaling_factor(self):
-        if self.model is None:
-            self.load_model()
-
-        if self.scaling_factor is None:
-            scaling = 1
-            for layer in self.model.layers:
-                config = layer.get_config()
-                if 'mp' in config['name']:
-                    scaling *= int(config['pool_size'][0])
-
-            print('Calculated scaling factor:',scaling)
-            self.scaling_factor = scaling
-
-        return self.scaling_factor
-
-    def get_kernel_padding(self):
-        if self.model is None:
-            self.load_model()
-
-        if self.kernel_padding is None:
-            for layer in self.model.layers:
-                config = layer.get_config()
-                if 'conv_fcn0' in config['name']:
-                    self.kernel_padding = (layer.input_shape[1] - 1)/2
-                    break
-            print('Calculated padding factor:',self.kernel_padding)
-
-        return self.kernel_padding
-
-    # Method to test the reshaping sanity of trainl labels for fully convolutional
-    def __test_reshape_trainlabels_for_fcv(self, labels_train):
-        test = np.reshape(labels_train,(labels_train.shape[0], 1, 1, labels_train.shape[1]))
-        print('Input shape: {0}'.format(labels_train.shape))
-        print('Output shape: {0}'.format(test.shape))
-        ok_reshape_logic = test[:, 0, 0, :] == labels_train
-        if np.all(ok_reshape_logic) == True:
-            print('Reshaped values \'labels_train\' for fully convolutional network successfully')
-        else:
-            print('Reshaped values \'labels_train\' for fully convolutional network failed')
 
     # patch_shape = (nrows, ncols, nchannels)
     def __build_graph(self, patch_shape, padding='same', fcn_head_row=None, fcn_head_col=None):
