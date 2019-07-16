6e953517ff44e356487334e23e7127d09c3dfec0
Georgia Rajamanoharan <georgia@users.noreply.github.com>
2019-07-16 11:03:39 +0100

    Allow the new test script to work on either rgb or grayscale images.

--- test_candidates_code.py
+++ test_candidates_code.py
@@ -16,6 +16,7 @@ def get_arguments():
     parser = argparse.ArgumentParser(description='Takes a pair of images and tests against a trained rotation CNN, returning the angle between them.')
     parser.add_argument('--test_data_file', type=str, required=True, help='Testing dataset HDF5 file (generated via create_data_set.py).')
     parser.add_argument('--results_file', type=str, required=True, help='Results file to save comparison true and predicted labels.')
+    parser.add_argument('--image_format', type=str, choices={'rgb','bw'}, default='rgb', help='Whether to test model on RGB or grayscale images.')
     return parser.parse_args()
 
 ### Main function ###
@@ -26,13 +27,22 @@ if __name__ == "__main__":
     test_data_f = h5py.File(args.test_data_file,'r')
     results_file = args.results_file
 
-    test_images = test_data_f['images_rgb']
+    if args.image_format == 'rgb':
+        test_images = test_data_f['images_rgb']
+        len_idx = 3
+    else:
+        test_images = test_data_f['images']
+        len_idx = 2
     test_labels = test_data_f['rotations']
 
-    angles = np.empty(shape=(test_images.shape[3],1))
-    for idx in range(test_images.shape[3]):
-        im1 = test_images[:100,:,:,idx]
-        im2 = test_images[100:,:,:,idx]
+    angles = np.empty(shape=(test_images.shape[len_idx],1))
+    for idx in range(test_images.shape[len_idx]):
+        if args.image_format == 'rgb':
+            im1 = test_images[:100,:,:,idx]
+            im2 = test_images[100:,:,:,idx]
+        else:
+            im1 = test_images[:100,:,idx]
+            im2 = test_images[100:,:,idx]
         angles[idx] = get_rotation(im1,im2)
 
     angles[angles < 0] = 360+angles[angles<0]
